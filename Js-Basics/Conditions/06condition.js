fa8b8da8249bf0d5878ff3d875add2ddf431950e
websubhamdevloper <156600798+websubhamdevloper@users.noreply.github.com>
2026-04-05 15:57:17 +0530

    add: more complex conditional programs

---
+++ Js-Basics/Conditions/06condition.js
@@ -0,0 +1,66 @@
+function checkJobEligiblity(userData) {
+  let result = {
+    eligible: false,
+    reason: [],
+    score: 0,
+  };
+
+  if (userData.age < 18) {
+    result.reason.push("Too young - minimum age is 18!");
+  } else if (userData.age > 65) {
+    result.reason.push("Too old - maximum age is 65");
+  } else {
+    result.score += 20;
+  }
+
+  if (userData.experience >= 2) {
+    result.score += 30;
+  } else if (userData.expreience >= 1) {
+    result.score += 15;
+  }
+
+  if (!userData.hasEducation) {
+    result.reason.push("Need at least 1 year experience OR relevant education");
+  } else if (userData.hasEducation) {
+    result.score += 25;
+  }
+
+  if (userData.hasSkills) {
+    result.score += 20;
+  } else {
+    result.reason.push("required job skills not demonstrated");
+  }
+
+  if (
+    userData.employmentStatus === "Employed" ||
+    userData.employmentStatus === "employed" ||
+    userData.employmentStatus === "freelancer"
+  ) {
+    result.score += 5;
+  }
+
+  if (result.score >= 70 && result.reason.length === 0) {
+    result.eligible = true;
+  }
+
+  return result;
+}
+
+// test case 1
+let candidate1 = {
+  age: 28,
+  experience: 3,
+  hasEducation: true,
+  hasSkills: true,
+  employmentStatus: "employed",
+};
+
+const CandidateResult = checkJobEligiblity(candidate1);
+
+console.log("Candidate eligible for the job: " + CandidateResult.eligible);
+if (CandidateResult.reason.length === 0) {
+  console.log("You have no reason to get rejected.");
+} else {
+  console.log("The reason for being rejected is ", CandidateResult.reason);
+}
+console.log("The total score is " + CandidateResult.score);
