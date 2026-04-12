34e25c53bd244477745ebf67deceeeac48fb7fc1
websubhamdevloper <156600798+websubhamdevloper@users.noreply.github.com>
2026-04-12 16:26:22 +0530

    add: varibale program in js-basics

---
+++ Js-Basics/Variables/var1.js
@@ -0,0 +1,35 @@
+var name = "subham kundu";
+console.log("var name is ", name);
+
+let age = 21;
+console.log("let age is ", age);
+
+const country = "India";
+console.log("const country:", country);
+
+var name2 = "viraj";
+name2 = "Raja";
+console.log("changed name is ", name2);
+
+let age2 = 20;
+age2 = 21;
+console.log("changed age is ", age2);
+
+const city = "siliguri";
+// city = "jalpaiguri";
+console.log("const city:", city);
+
+let x = 10,
+  y = 20,
+  z = 30;
+console.log("Multiple let declaration: ", x, z, y);
+
+const a = 100,
+  b = 300;
+console.log("Multiple const declaration:", a, b);
+
+let uninitialized;
+console.log("let uninitialized", uninitialized);
+
+const initialized = "must initialize";
+console.log("const initialize", initialized);
